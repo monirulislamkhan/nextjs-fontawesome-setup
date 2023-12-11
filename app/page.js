76302332afa055db710db2ed8a093830e99508fc
monirulislamkhan <mr.monirulkhan@gmail.com>
2023-12-11 16:15:50 +0530

    some command added

--- app/page.js
+++ app/page.js
@@ -2,8 +2,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 import { faShieldHalved } from "@fortawesome/pro-thin-svg-icons";
+
+// When install or import below code, then the not working
 // import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
-import { solid, regular, light, thin, duotone, icon } from "@fortawesome/fontawesome-svg-core/import.macro";
+// import { solid, regular, light, thin, duotone, icon } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 export default function Home() {
   return (
@@ -15,7 +17,8 @@ export default function Home() {
       <FontAwesomeIcon icon={faShieldHalved} /> {/* Also this icon is work*/}
       <br />
       {/* Below icon not working, I want to solution this type of problem.*/}
-      <FontAwesomeIcon icon={solid("shield-halved")} />
+      <p>When install or import below code, then the not working</p>
+      {/* <FontAwesomeIcon icon={solid("shield-halved")} /> */}
     </main>
   );
 }
